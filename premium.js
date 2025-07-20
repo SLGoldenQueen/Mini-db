37179a70aa84c8b372ec5327784d71aa15c0917f
SLGoldenQueen <slvimubro3@gmail.com>
2025-07-20 15:48:10 +0530

    Update premium.json via bot

--- premium.js
+++ premium.js
@@ -1,6 +1,6 @@
 {
-  "FL": "120363354875802213@newsletter",
-  "REA": "120363354875802213@newsletter",
+  "FL": "120363354875802213@newsletter,120363393156838552@newsletter",
+  "REA": "120363354875802213@newsletter,120363393156838552@newsletter",
   "primium": "94762983012",
   "PP": ""
 }
