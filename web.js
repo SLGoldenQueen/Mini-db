9132401da0051c9c94ca891e99b3a20bba394e2d
SLGoldenQueen <slvimubro3@gmail.com>
2025-08-03 15:47:25 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,7 +1,5 @@
 {
-"Img1":"https://i.ibb.co/Ld1rvZP9/0b1709abb013.jpg",
+"Img1":"https://i.ibb.co/DgfHMR5v/a8bdfecefed6.jpg",
 "Img2":"https://i.ibb.co/Ld1rvZP9/0b1709abb013.jpg",
-"Img3":"https://i.ibb.co/Ld1rvZP9/0b1709abb013.jpg",
-"Img4":"https://i.ibb.co/Ld1rvZP9/0b1709abb013.jpg",
-"Img5":"https://i.ibb.co/Ld1rvZP9/0b1709abb013.jpg"
+"Img3":"https://i.ibb.co/cXTyNsGg/a310f7c90e7b.jpg"
 }
