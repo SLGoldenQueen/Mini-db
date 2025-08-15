c67c2eebec36630694f6b40297dfb234cbbc9b6c
SLGoldenQueen <slvimubro3@gmail.com>
2025-08-15 18:06:32 +0530

    Update 94703781273 settings via bot

--- settings/94703781273.js
+++ settings/94703781273.js
@@ -17,7 +17,7 @@
   "AUTO_READ_STATUS": "true",
   "AUTO_READ_MSG": "false",
   "AUTO_SAVE": "false",
-  "CMD_READ": "false",
+  "CMD_READ": "true",
   "AUTO_VOICE": "true",
   "AUTO_BLOCK": "false",
   "OWNER_IMG": "https://files.catbox.moe/wdzt28.jpg",
