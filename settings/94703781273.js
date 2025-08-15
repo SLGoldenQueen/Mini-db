e14a5029bae0fd860e1190728b4f6127ca199802
SLGoldenQueen <slvimubro3@gmail.com>
2025-08-15 20:01:35 +0530

    Update 94703781273 settings via bot

--- settings/94703781273.js
+++ settings/94703781273.js
@@ -5,7 +5,7 @@
   "PRIFIX": ".",
   "MODE": "public",
   "VIMA_LAN": "SI",
-  "AUTO_REACT": "false",
+  "AUTO_REACT": "true",
   "ANTI_DELETE": "true",
   "ANTI_CALL": "false",
   "CALL_REJECT_LIST": "",
