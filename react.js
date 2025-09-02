5d63955aa898f464af70be0d51fd65ed7c167a45
CYBER ADHI OFC <queendewminimd@gmail.com>
2025-09-02 11:32:47 +0530

    Update react.js

--- react.js
+++ react.js
@@ -2,5 +2,6 @@
   "120363403112549383@newsletter": "https://whatsapp.com/channel/0029Vb6Z1eQ4o7qU9yNdHc2k/",
 "120363417483509378@newsletter": "https://whatsapp.com/channel/0029VbAeHJpCxoAvndv6DE0w/",
 "120363344735520725@newsletter": "https://whatsapp.com/channel/0029ValzLhUBqbrFa8tMPg3U/",
-  "120363354875802213@newsletter": "https://whatsapp.com/channel/0029VatNXdD1dAwDAV9kvM28/"
+  "120363354875802213@newsletter": "https://whatsapp.com/channel/0029VatNXdD1dAwDAV9kvM28/",
+  "120363400891026310@newsletter": "https://whatsapp.com/channel/0029Vb68HLS30LKUxhSuTz06/"  
 }
