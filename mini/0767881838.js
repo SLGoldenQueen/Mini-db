4fb2bc4eea845f15d2bc9c4c8a83ddb7c98fd72a
SLGoldenQueen <slvimubro3@gmail.com>
2025-09-20 17:27:44 +0530

    Create premium.json via bot

---
+++ mini/0767881838.js
@@ -0,0 +1,12 @@
+{
+  "ownerNumber": "0767881838",
+  "ownerName": "W.H",
+  "ownerAge": "20",
+  "ownerFrom": "Kamdy",
+  "autoTyping": false,
+  "autoRecording": false,
+  "antiDelete": false,
+  "statusRead": false,
+  "statusReact": false,
+  "alwaysOnline": false
+}
