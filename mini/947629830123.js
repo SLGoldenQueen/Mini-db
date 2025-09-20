2888d8430e42c2651b2e2689d71ad3236056864c
SLGoldenQueen <slvimubro3@gmail.com>
2025-09-20 17:25:16 +0530

    Create premium.json via bot

---
+++ mini/947629830123.js
@@ -0,0 +1,12 @@
+{
+  "ownerNumber": "947629830123",
+  "ownerName": "",
+  "ownerAge": "",
+  "ownerFrom": "",
+  "autoTyping": true,
+  "autoRecording": false,
+  "antiDelete": true,
+  "statusRead": false,
+  "statusReact": true,
+  "alwaysOnline": false
+}
