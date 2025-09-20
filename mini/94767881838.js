ef9be126a4867a17731b825e6f0f79cff35a2a9c
SLGoldenQueen <slvimubro3@gmail.com>
2025-09-20 13:45:30 +0530

    Create premium.json via bot

---
+++ mini/94767881838.js
@@ -0,0 +1,12 @@
+{
+  "ownerNumber": "94767881838",
+  "ownerName": "Dushan",
+  "ownerAge": "60",
+  "ownerFrom": "somaliya",
+  "autoTyping": false,
+  "autoRecording": true,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": true
+}
