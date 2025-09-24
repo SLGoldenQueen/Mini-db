a942d77472d0fe7ebb1974a79993545df171a1ec
SLGoldenQueen <slvimubro3@gmail.com>
2025-09-24 22:44:21 +0530

    Create premium.json via bot

---
+++ mini/+94 72 031 9480.js
@@ -0,0 +1,12 @@
+{
+  "ownerNumber": "+94 72 031 9480",
+  "ownerName": "",
+  "ownerAge": "",
+  "ownerFrom": "",
+  "autoTyping": true,
+  "autoRecording": false,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": true
+}
