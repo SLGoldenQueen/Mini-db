ebf540691db8349b18f3826f2bf7f94d7ed9148d
SLGoldenQueen <slvimubro3@gmail.com>
2025-09-25 10:07:13 +0530

    Create premium.json via bot

---
+++ mini/94722474119y.js
@@ -0,0 +1,12 @@
+{
+  "ownerNumber": "94722474119y",
+  "ownerName": "Vima",
+  "ownerAge": "555",
+  "ownerFrom": "Galle",
+  "autoTyping": true,
+  "autoRecording": false,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "alwaysOnline": false
+}
