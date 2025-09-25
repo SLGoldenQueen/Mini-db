1bccf4f2b50099fd096bfbc5793e3885cea66405
SLGoldenQueen <slvimubro3@gmail.com>
2025-09-25 10:29:16 +0530

    Update premium.json via bot

--- mini/94722474119.js
+++ mini/94722474119.js
@@ -1,10 +1,10 @@
 {
   "ownerNumber": "94722474119",
   "ownerName": "Vima",
-  "ownerAge": "555",
+  "ownerAge": "4",
   "ownerFrom": "Galle",
-  "autoTyping": true,
-  "autoRecording": false,
+  "autoTyping": false,
+  "autoRecording": true,
   "antiDelete": true,
   "statusRead": true,
   "statusReact": true,
